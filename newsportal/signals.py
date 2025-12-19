8a8e479617c2372cf314d9333004ae62b7176d3d
Name <email@example.ru>
2025-12-19 23:27:31 +0300

    Remove unused HTML welcome email template

--- newsportal/signals.py
+++ newsportal/signals.py
@@ -1,12 +1,9 @@
 from django.contrib.auth.models import Group
-from django.db.models.signals import post_save
 from django.core.mail import EmailMessage
 from django.core.exceptions import ValidationError
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
-from django.contrib.sites.shortcuts import get_current_site
-from django.urls import reverse
 from django.utils.timezone import now
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
@@ -19,6 +16,8 @@ from .models import News
 
 User = get_user_model()
 
+
+
 @receiver(pre_save, sender=News)
 def news_pre_save(sender, instance, *args, **kwargs):
     if instance.pk:
@@ -26,71 +25,79 @@ def news_pre_save(sender, instance, *args, **kwargs):
 
     today = now().date()
 
-    news_count = News.objects.filter(author=instance.author,
-                                     created_at__date=today
-                                     ).count()
-    if news_count >= 6:
-        raise ValidationError("Вы не можете публиковать более 3 новостей в сутки.")
+    news_count = News.objects.filter(
+        author=instance.author,
+        created_at__date=today
+    ).count()
+
+    if news_count >= 3:
+        raise ValidationError(
+            "Вы не можете публиковать более 3 новостей в сутки."
+        )
+
 
 @receiver(post_save, sender=User)
 def create_user_to_common(sender, instance, created, **kwargs):
     if created:
-        group = Group.objects.get(name='common')
-        instance.groups.add(group)
+        try:
+            group = Group.objects.get(name='common')
+            instance.groups.add(group)
+        except Group.DoesNotExist:
+            pass
+
 
 @receiver(post_save, sender=User)
 def send_welcome_email(sender, instance, created, **kwargs):
     if not created:
         return
-    instance.is_active = False
-    instance.save()
+
+
+    User.objects.filter(pk=instance.pk).update(is_active=False)
 
     token = default_token_generator.make_token(instance)
     uid = urlsafe_base64_encode(force_bytes(instance.pk))
 
     activation_link = f"{settings.SITE_URL}/activate/{uid}/{token}/"
-    html_content = render_to_string('welcome_email.html', {
-        'username': instance.username,
-        'activation_link': activation_link,
-    })
-    msg = EmailMultiAlternatives(
-        'Добро пожаловать!',
-        '',
-        settings.EMAIL_HOST_USER,
-        [instance.email]
+
+    # plain-text (стабильно для email)
+    text_content = (
+        f"Здравствуй, {instance.username}!\n\n"
+        f"Добро пожаловать на сайт.\n\n"
+        f"Для активации аккаунта перейдите по ссылке:\n"
+        f"{activation_link}"
     )
-    msg.attach_alternative(html_content, "text/html")
+
+    msg = EmailMessage(
+        subject="Добро пожаловать!",
+        body=text_content,
+        from_email=settings.EMAIL_HOST_USER,
+        to=[instance.email],
+    )
     msg.send()
+
 
 @receiver(post_save, sender=News)
 def notify_subscribers(sender, instance, created, **kwargs):
-    print("✅ SIGNAL TRIGGERED")
     if not created or not instance.category:
         return
 
-    subscribers = instance.category.subscribers.all()
-    print("📩 SUBSCRIBERS:", list(subscribers))
     article_url = f"{settings.SITE_URL}{instance.get_absolute_url()}"
+    subscribers = instance.category.subscribers.all()
 
     for user in subscribers:
         if not user.email:
             continue
 
-
-
         text_content = (
             f"Здравствуй, {user.username}!\n\n"
             f"В твоём любимом разделе вышла новая статья.\n\n"
             f"{instance.text[:150]}...\n\n"
-            f"Читать полностью: {article_url}"
+            f"Читать полностью:\n{article_url}"
         )
 
-
-        msg = EmailMessage(
+        EmailMessage(
             subject=instance.title,
             body=text_content,
             from_email=settings.EMAIL_HOST_USER,
-            to=[user.email]
-        )
-
-        msg.send()
+            to=[user.email],
+        ).send()
